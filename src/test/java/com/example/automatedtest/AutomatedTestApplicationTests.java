f8675a427d0348e220f9ccac2d1b641c2dbc7653
KEATTLoevkilde <thom4a14@stud.kea.dk>
2023-04-24 13:22:39 +0200

    Initial commit

---
+++ src/test/java/com/example/automatedtest/AutomatedTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.automatedtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
